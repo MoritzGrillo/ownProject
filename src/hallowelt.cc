917f9847681b2ccc0f8effcf3d50be690c5bd292
MoritzGrillo <mg166257@uni-greifswald.de>
2019-04-03 16:33:25 +0200

    name

--- src/hallowelt.cc
+++ src/hallowelt.cc
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char** argv){
 
     enum languages {german, english, french};
-    string greetings[3] = {"Hallo Welt!", "Hello world!", "Salut monde!"};
+    string greetings[3] = {"Hallo Moritz!", "Hello Moritz!", "Salut Moritz!"};
     languages lang = french;
     cout << greetings[lang] << endl;
 	return 0;
