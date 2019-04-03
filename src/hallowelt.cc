6ae4cd866679f24f6c424d0abd8832341a3d030f
MoritzGrillo <mg166257@uni-greifswald.de>
2019-04-03 16:35:28 +0200

    master added spanish

--- src/hallowelt.cc
+++ src/hallowelt.cc
@@ -5,8 +5,8 @@ using namespace std;
 
 int main(int argc, char** argv){
 
-    enum languages {german, english, french};
-    string greetings[3] = {"Hallo Welt!", "Hello world!", "Salut monde!"};
+    enum languages {german, english, french, spanish};
+    string greetings[4] = {"Hallo Welt!", "Hello world!", "Salut monde!", "Hola mundo!"};
     languages lang = french;
     cout << greetings[lang] << endl;
 	return 0;
